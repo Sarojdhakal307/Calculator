cf768c1f5319dba4ea8400514659bc8dd7981fe7
SAROJ DHAKAL <134726799+Sarojdhakal307@users.noreply.github.com>
2024-04-20 22:54:25 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+var output = document.querySelector(".output");
+var buttonsnum = document.querySelectorAll(".btnnum");
+var clear = document.querySelector(".clear");
+var btndec = document.querySelector(".btndec");
+var buttonop = document.querySelectorAll(".buttonop");
+var output1 = document.querySelector(".output1");
+var equal = document.querySelector(".equal");
+
+let currentVal = "";
+let firstVal = null;
+let operator = null;
+let btndecidx = 0;
+let opidx = 0;
+let btnopidex = 0;
+let result = null;
+
+function calculate() {
+  console.log(operator);
+  
+  switch (operator) {
+    case "+":
+      result = firstVal + parseFloat(currentVal);
+      break;
+    case "-":
+      result = firstVal - parseFloat(currentVal);
+      break;
+    case "*":
+      result = firstVal * parseFloat(currentVal);
+      break;
+    case "/":
+      result = firstVal / parseFloat(currentVal);
+      break;
+    default:
+      break;
+  }
+  currentVal = result;
+  firstVal = result;
+  output1.value = currentVal;
+}
+function display(e) {
+  output.value += e.target.textContent; // Use 'this' to reference the clicked button
+  console.log("I am clicked");
+  btnopidex = 0;
+}
+btndec.addEventListener('click', function(e){
+  if(btndecidx === 0){
+      output.value += this.value;
+      btndecidx = 1;
+    }
+});
+buttonop.forEach(buttonop => {
+  buttonop.addEventListener('click',function(e){
+
+    if(opidx===0){
+      firstVal = parseFloat(output.value);
+      opidx = 1;
+    }
+    else{
+      currentVal = parseFloat(output.value);
+      calculate();
+      output.value = result;
+      output1.value = result;
+      output.value = "";
+    }
+   if(btnopidex===0){ 
+    console.log(e);
+    operator = e.target.value;
+    output.value += this.value;
+    btndecidx = 0;
+    btnopidex=1;
+    output1.value += output.value;
+    output.value = "";
+  }
+ });
+});
+clear.addEventListener('click', function(e){
+ 
+  currentVal = "";
+  firstVal = null;
+  operator = null;
+  output.value = "";
+  output1.value = "";
+
+  btndecidx = 0;
+  console.log(output.value)
+});
+buttonsnum.forEach(buttonsnum => {
+  buttonsnum.addEventListener('click', display);
+});
+equal.addEventListener('click', function(e){
+  currentVal = parseFloat(output.value);
+  calculate();
+  output.value = result;
+  output1.value = "";
+  firstVal = null;
+  operator = null;
+  btndecidx = 0;
+  opidx = 0;
+  btnopidex = 0;
+});
